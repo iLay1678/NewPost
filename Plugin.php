fec443b47d79f8fd6e3eb3c05f71be2453074593
iLay1678 <807190135@qq.com>
2019-04-17 10:25:41 +0800

    更多参数自定义

--- Plugin.php
+++ Plugin.php
@@ -1,72 +1,73 @@
 <?php
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-/**
- * 通过POST或GET发送文章
- * 
- * @package NewPost
- * @author ilay
- * @version 0.1
- * @link https://wei.bz
- */
-class NewPost_Plugin implements Typecho_Plugin_Interface {
-   /**
-     * 激活插件方法,如果激活失败,直接抛出异常
-     * 
-     * @access public
-     * @return void
-     * @throws Typecho_Plugin_Exception
-     */
-    public static function activate(){
-      Helper::addAction("import", "NewPost_Action");
-      return "请设置sign值";
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
+class NewPost_Action extends Widget_Abstract_Contents implements Widget_Interface_Do
+{
+    public function __construct($request, $response, $params = NULL) {
+        parent::__construct($request, $response, $params);
     }
-    
-    /**
-     * 禁用插件方法,如果禁用失败,直接抛出异常
-     * 
-     * @static
-     * @access public
-     * @return void
-     * @throws Typecho_Plugin_Exception
-     */
-    public static function deactivate(){
-     Helper::removeAction('import');
+
+    public function action() {
+      $options = Typecho_Widget::widget('Widget_Options')->plugin('NewPost');
+        $user=$options->username;
+        $password=$options->password;
+        if (!$this->user->hasLogin()) {
+            if (!$this->user->login($user, $password, true)) { //使用特定的账号登陆
+                die('登录失败');
+            }
+        }
+
+        $request = Typecho_Request::getInstance();
+        $title = $request->get('title');
+        $text = $request->get('text');
+        $key= $request->get('key');
+        $fieldnames=$request->get('fieldnames');
+        $fieldvalues=$request->get('fieldvalues');
+        $fieldtypes=$request->get('fieldtypes');
+        $signkey = $options->sign;
+        $mid = $request->get('mid');
+      	if($key!=$signkey){
+        die("验证失败");
+        }
+
+        //填充文章的相关字段信息。
+        $request->setParams(
+            array(
+                'title'=>$title,
+                'text'=>$text,
+                'fieldNames'=>unserialize($fieldnames),
+                'fieldTypes'=>unserialize($fieldtypes),
+                'fieldValues'=>unserialize($fieldvalues),
+                'cid'=>'',
+                'do'=>'publish',
+                'markdown'=>'1',
+                'date'=>'',
+                'category'=>unserialize($mid),
+                'tags'=>'',
+                'visibility'=>'publish',
+                'password'=>'',
+                'allowComment'=>'1',
+                'allowPing'=>'1',
+                'allowFeed'=>'1',
+                'trackback'=>'',
+            )
+        );
+
+        //设置token，绕过安全限制
+        $security = $this->widget('Widget_Security');
+        $request->setParam('_', $security->getToken($this->request->getReferer()));
+        //设置时区，否则文章的发布时间会查8H
+        date_default_timezone_set('PRC');
+
+        //执行添加文章操作
+        $widgetName = 'Widget_Contents_Post_Edit';
+        $reflectionWidget = new ReflectionClass($widgetName);
+        if ($reflectionWidget->implementsInterface('Widget_Interface_Do')) {
+            $this->widget($widgetName)->action();
+            echo 'Successful';
+            return;
+        }
     }
-    
-    /**
-     * 获取插件配置面板
-     * 
-     * @access public
-     * @param Typecho_Widget_Helper_Form $form 配置面板
-     * @return void
-     */
-    public static function config(Typecho_Widget_Helper_Form $form){
-        echo '<p>首先添加一位用户，用户组为 编辑 ，把账号密码填在下面。<p>key为验证密钥</p><p>mid为要通过post发送的文章分类,例如某分类设置页url为 https://wei.bz/admin/category.php?mid=2 ，mid就等于2</p>';
-      echo '通过post请求 http://你的域名/action/import 参数：title 标题 ;  text 正文内容;  key 验证密钥</p>';
-       $username = new Typecho_Widget_Helper_Form_Element_Text('username', NULL, _t(''), _t('username'));
-    	$form->addInput($username);
-      $password = new Typecho_Widget_Helper_Form_Element_Text('password', NULL, _t(''), _t('password'));
-    	$form->addInput($password);
-    $key = new Typecho_Widget_Helper_Form_Element_Text('sign', NULL, _t('123456'), _t('key'));
-      $form->addInput($key);
-    $mid = new Typecho_Widget_Helper_Form_Element_Text('mid', NULL, _t(''), _t('mid'));
-    	$form->addInput($mid);
-    }
-    
-    /**
-     * 个人用户的配置面板
-     * 
-     * @access public
-     * @param Typecho_Widget_Helper_Form $form
-     * @return void
-     */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
-    
-    /**
-     * 插件实现方法
-     * 
-     * @access public
-     * @return void
-     */
-  
 }
